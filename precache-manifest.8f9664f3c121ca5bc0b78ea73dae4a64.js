c2b50b145d31c53d0b5c38095e38d652e9b27155
arshia <arshiamd.777@gmail.com>
2020-08-05 13:10:56 +0530

    Updates

---
+++ precache-manifest.8f9664f3c121ca5bc0b78ea73dae4a64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe21a9cadcab6ac1ec36d434943f2934",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "fa289776c3fdf07bc3ab",
+    "url": "/demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "27d2ef10de27b3307a04",
+    "url": "/demo/static/js/2.53bd5559.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo/static/js/2.53bd5559.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa289776c3fdf07bc3ab",
+    "url": "/demo/static/js/main.0463614f.chunk.js"
+  },
+  {
+    "revision": "b39b67791598b779560b",
+    "url": "/demo/static/js/runtime-main.a819b733.js"
+  }
+]);
